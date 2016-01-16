23b982e68a22d527b7be6a8b6acf59ed10707a69
Darren Burgess <darren_b@beezwax.net>
2016-01-16 08:09:28 -0500

    add main loop and refactor

--- lesson_02/calculator.rb
+++ lesson_02/calculator.rb
@@ -25,27 +25,36 @@ def get_input(message, type)
   end
 end
 
-a  = get_input('Enter first number', :num)
-b  = get_input('Enter second number', :num)
-op = get_input('Select an operator (*, /, +, -)', :op)
+def get_result(a, b, op)
+  case op
+  when '*'
+    a * b
+  when '/'
+    if b == 0.0
+      'Divide by zero error'
+    else
+      a.to_f / b.to_f
+    end
+  when '+'
+    a + b
+  when '-'
+    a - b
+  end
+end
 
-result =  case op
-          when '*'
-            a * b
-          when '/'
-            if b == 0.0
-              'Divide by zero error'
-            else
-              a.to_f / b.to_f
-            end
-          when '+'
-            a + b
-          when '-'
-            a - b
-          end
+loop do
+  a      = get_input('Enter first number', :num)
+  b      = get_input('Enter second number', :num)
+  op     = get_input('Select an operator (*, /, +, -)', :op)
+  result = get_result(a, b, op)
 
-if result != 'Divide by zero error'
-  prompt("#{a.to_s + op + b.to_s} = #{result}")
-else
-  prompt(result)
+  if result != 'Divide by zero error'
+    prompt("#{a.to_s + op + b.to_s} = #{result}")
+  else
+    prompt(result)
+  end
+
+  prompt('Calculate again? Y or N')
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end

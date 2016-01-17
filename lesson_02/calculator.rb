e48521ce20640b17a981727afc76598195be8673
Darren Burgess <darren_b@beezwax.net>
2016-01-16 19:40:19 -0500

    Refactor calculator: - improve number validation - separate operator validation - improve variable names - improve float/integer handling

--- lesson_02/calculator.rb
+++ lesson_02/calculator.rb
@@ -7,43 +7,54 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def valid_input?(input, type)
-  if type == :num
-    # small issue input = '1asdf' will validate as '1asdf'.to_i = 1
-    input == '0' || input == '0.0' || input.to_i != 0
+def valid_number?(input)
+  input.to_i.to_s == input || input.to_f.to_s == input
+end
+
+def convert_to_number(input)
+  if input.to_i.to_s == input
+    input.to_i
   else
-    input =~ /^[-+*\/]$/ # one character in the accepted operators
+    input.to_f
   end
+end
+
+def valid_operator?(input)
+  %w(+ - * /).include?(input) && input.length == 1
 end
 
 def get_input(message, type)
   prompt(message)
   loop do
     input = gets.chomp
-    if valid_input?(input, type)
-      return type == :num ? input.to_f : input
+    if type == :number && valid_number?(input)
+      return input
+    elsif type == :operator && valid_operator?(input)
+      return input
     else
       prompt(MESSAGES['invalid_input'])
     end
   end
 end
 
-def get_result(a, b, op)
-  if op == '+' || op == '-' || op == '*'
-    eval("#{a} #{op} #{b}") # yes, eval is evil
+def calculate_result(first_number, second_number, operator)
+  first  = convert_to_number(first_number)
+  second = convert_to_number(second_number)
+  if %w(+ - *).include?(operator)
+    first.send(operator, second)
   else
-    b == 0.0 ? 'Divide by zero error' : a.to_f / b.to_f
+    second == 0.0 ? 'Divide by zero error' : first.to_f / second.to_f
   end
 end
 
 loop do
-  a      = get_input(MESSAGES['input_first_number'], :num)
-  b      = get_input(MESSAGES['input_second_number'], :num)
-  op     = get_input(MESSAGES['input_operator'], :op)
-  result = get_result(a, b, op)
+  first_number  = get_input(MESSAGES['input_first_number'], :number)
+  second_number = get_input(MESSAGES['input_second_number'], :number)
+  operator      = get_input(MESSAGES['input_operator'], :operator)
+  result        = calculate_result(first_number, second_number, operator)
 
   if result != 'Divide by zero error'
-    prompt("#{a.to_s + op + b.to_s} = #{result}")
+    prompt("#{first_number}#{operator}#{second_number} = #{result}")
   else
     prompt(result)
   end
